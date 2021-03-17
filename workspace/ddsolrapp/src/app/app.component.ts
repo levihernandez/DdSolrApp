e5f197f01ccb769cab7e94323998d96a76c1d509
jlhernandez <jpnoles@gmail.com>
2021-03-17 15:12:20 -0400

    Added Datadog RUM code location + instructions

--- workspace/ddsolrapp/src/app/app.component.ts
+++ workspace/ddsolrapp/src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
 
+/*
+// Create a new RUM Application in Datadog UI: https://app.datadoghq.com/rum/application
+// Copy the npm Code snippet and inject below
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '<Datadog-app-applicationId>',
+    clientToken: '<Datadog-app-clientToken>',
+    site: 'datadoghq.com',
+    service: 'NodeJS-Solr',
+    env: 'production',
+    version: '1.0.0',
+    sampleRate: 100,
+    trackInteractions: true
+});
+*/
+
 
 @Component({
   selector: 'app-root',
